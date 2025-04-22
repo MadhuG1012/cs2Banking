89e87a7776c902c400882e2f205875b13139ef4e
Madhusmita Sahoo <masahoo4@in.ibm.com>
2025-04-22 15:25:38 +0000

    commit

---
+++ models/xml_data/xmlcs2.sql
@@ -0,0 +1,9 @@
+SELECT
+XMLGET(s.value, 'Transaction_ID'):"$"::string as "Transaction_ID",
+XMLGET(s.value, 'Customer_ID'):"$"::int as "Customer_ID",
+XMLGET(s.value, 'Transaction_Date'):"$"::date as "Transaction_Date",
+XMLGET(s.value, 'Transaction_Type'):"$"::string as "Transaction_Type",
+XMLGET(s.value, 'Transaction_Amount'):"$"::int as "Transaction_Amount",
+XMLGET(s.value, 'Transaction_Mode'):"$"::string as "Transaction_Mode"
+FROM MYDB.XMLCS2.CS_2_BANKING,
+LATERAL FLATTEN(TO_ARRAY(MYDB.XMLCS2.CS_2_BANKING._DATA:"$")) s
